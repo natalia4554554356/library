725021009a291bfa97bafd3a593ad46e47ae084e
Bezpala Natalia <bezpala.nataliia1621@vu.cdu.edu.ua>
2023-05-10 11:27:22 +0300

    Creating saving script for editor page

---
+++ public/editor.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
+    authDomain: "e-library-aace8.firebaseapp.com",
+    projectId: "e-library-aace8",
+    storageBucket: "e-library-aace8.appspot.com",
+    messagingSenderId: "535323215968",
+    appId: "1:535323215968:web:8273a7055ab0922a5adb05"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+const form = document.getElementById("book-form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("book-author").value;
+    const type = document.getElementById("book-type").value;
+    const pages = parseInt(document.getElementById("book-pages").value);
+    const year = parseInt(document.getElementById("book-year").value);
+    const isbn = document.getElementById("book-isbn").value;
+    const imageFile = document.getElementById("book-image").files[0];
+
+    const imageName = Date.now() + "_" + imageFile.name;
+    const imageRef = ref(storage, imageName);
+    await uploadBytes(imageRef, imageFile);
+
+    const imageUrl = await getDownloadURL(imageRef);
+
+    const bookData = {
+        title,
+        author,
+        type,
+        pages,
+        year,
+        isbn,
+        imageUrl,
+        createdAt: serverTimestamp(),
+    };
+    await addDoc(collection(db, "books"), bookData);
+
+    form.reset();
+});
+
