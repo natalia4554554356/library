376e66acf58b1f963dce5db5fa243c8e2bc3c866
Bezpala Natalia <bezpala.nataliia1621@vu.cdu.edu.ua>
2023-05-11 14:49:11 +0300

    Created header link uploading from firestore on sign pages

--- public/sign_in.js
+++ public/sign_in.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import {getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import {getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
@@ -11,7 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+const db = getFirestore(app);
 const auth = getAuth(app);
 function logIn(email, password) {
     signInWithEmailAndPassword(auth, email, password)
@@ -24,7 +25,20 @@ function logIn(email, password) {
         });
 }
 
+async function fillHeader() {
+    const querySnapshot = await getDocs(collection(db, 'nav_items'));
+    const nav_items = {}
+    querySnapshot.forEach(doc => nav_items[`${doc.id}`] = doc.data())
+
+    const items = nav_items['auth_page'];
+    nav_list.innerHTML += '\n' + items['home'];
+    nav_list.innerHTML += '\n' + items['about_us'];
+}
+
 const form = document.getElementById('auth-form');
+const nav_list = document.getElementById('nav_list');
+
+fillHeader()
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
--- public/sign_up.js
+++ public/sign_up.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import {getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const auth = getAuth(app);
 
@@ -25,7 +27,19 @@ function signUp(email, password) {
         });
 }
 
+async function fillHeader() {
+    const querySnapshot = await getDocs(collection(db, 'nav_items'));
+    const nav_items = {}
+    querySnapshot.forEach(doc => nav_items[`${doc.id}`] = doc.data())
+
+    const items = nav_items['auth_page'];
+    nav_list.innerHTML += '\n' + items['home'];
+    nav_list.innerHTML += '\n' + items['about_us'];
+}
+
 const form = document.getElementById('auth-form');
+const nav_list = document.getElementById('nav_list');
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -39,3 +53,5 @@ form.addEventListener('submit', (e) => {
     else
         alert('Passwords are not equal')
 })
+
+fillHeader();
