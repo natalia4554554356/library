9c9f87b7e9f270a1da676314b0f5e97253101fdc
Bezpala Natalia <bezpala.nataliia1621@vu.cdu.edu.ua>
2023-05-13 11:27:57 +0300

    Initiated account page

---
+++ public/account.js
@@ -0,0 +1,34 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import {getFirestore, collection, getDocs} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
+    authDomain: "e-library-aace8.firebaseapp.com",
+    projectId: "e-library-aace8",
+    storageBucket: "e-library-aace8.appspot.com",
+    messagingSenderId: "535323215968",
+    appId: "1:535323215968:web:8273a7055ab0922a5adb05"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+fillHeader();
+
+async function fillHeader() {
+    const querySnapshot = await getDocs(collection(db, 'nav_items'));
+    const nav_items = {}
+    querySnapshot.forEach(doc => nav_items[`${doc.id}`] = doc.data())
+
+    nav_list.innerHTML = '';
+
+    const items = nav_items['account'];
+    nav_list.innerHTML += '\n' + items['home'];
+    nav_list.innerHTML += '\n' + items['account'];
+    nav_list.innerHTML += '\n' + items['about_us'];
+}
+
+
+const nav_list = document.getElementById('nav_list');
