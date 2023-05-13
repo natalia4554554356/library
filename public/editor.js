1c3e2d6da5cf7032f3f01b2f986103e356936a33
Bezpala Natalia <bezpala.nataliia1621@vu.cdu.edu.ua>
2023-05-14 00:25:07 +0300

    Added script to write to data book user id this book created

--- public/editor.js
+++ public/editor.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-storage.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
@@ -17,18 +18,30 @@ const db = getFirestore(app);
 
 const storage = getStorage(app);
 
-const form = document.getElementById("book-form");
+const auth = getAuth(app);
+let user;
+
+window.onload = () => {
+    onAuthStateChanged(auth, () => {
+        user = auth.currentUser;
+    })
+}
+
+const form = document.getElementById("book_create_form");
+
+fillHeader();
+
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    const title = document.getElementById("book-title").value;
-    const author = document.getElementById("book-author").value;
-    const type = document.getElementById("book-type").value;
-    const pages = parseInt(document.getElementById("book-pages").value);
-    const year = parseInt(document.getElementById("book-year").value);
-    const isbn = document.getElementById("book-isbn").value;
-    const imageFile = document.getElementById("book-image").files[0];
+    const title = document.getElementById("book_title").value;
+    const author = document.getElementById("book_author").value;
+    const type = document.getElementById("book_type").value;
+    const pages = parseInt(document.getElementById("book_pages").value);
+    const year = parseInt(document.getElementById("book_year").value);
+    const isbn = document.getElementById("book_isbn").value;
+    const imageFile = document.getElementById("book_image_upload_input").files[0];
 
     const imageName = Date.now() + "_" + imageFile.name;
     const imageRef = ref(storage, imageName);
@@ -36,7 +49,6 @@ form.addEventListener("submit", async (event) => {
 
     const imageUrl = await getDownloadURL(imageRef);
     const date = (new Date()).toString().split('(')[0];
-
     const bookData = {
         title,
         author,
@@ -47,9 +59,25 @@ form.addEventListener("submit", async (event) => {
         imageUrl,
         createdAt: date,
         updatedAt: date,
+        userID: user.uid,
     };
-    await addDoc(collection(db, "books"), bookData);
+    addDoc(collection(db, "books"), bookData).then(() => {
+        location = '/';
+    });
 
-    form.reset();
 });
 
+async function fillHeader() {
+    const querySnapshot = await getDocs(collection(db, 'nav_items'));
+    const nav_items = {}
+    querySnapshot.forEach(doc => nav_items[`${doc.id}`] = doc.data())
+
+    nav_list.innerHTML = '';
+
+    const items = nav_items['editor'];
+    nav_list.innerHTML += '\n' + items['home'];
+    nav_list.innerHTML += '\n' + items['account'];
+    nav_list.innerHTML += '\n' + items['about_us'];
+}
+
+const nav_list = document.getElementById('nav_list');
