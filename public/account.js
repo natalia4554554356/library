81086f672fcad95b40c47241daef44a816caaf44
Bezpala Natalia <bezpala.nataliia1621@vu.cdu.edu.ua>
2023-05-13 15:17:30 +0300

    Added list of liked books

--- public/account.js
+++ public/account.js
@@ -1,6 +1,6 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import {getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
-import {getFirestore, collection, getDocs} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import {getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import {getFirestore, collection, getDocs, getDoc, doc, deleteDoc, setDoc} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCa7D4i0-g9OQvsOHDefAlhcBl0neUZ2sY",
@@ -14,14 +14,113 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-
 let currentUser;
 
 onAuthStateChanged(auth, (user) => {
     currentUser = user;
+    fetchBooks();
 })
 
 fillHeader();
+
+
+async function fetchBooks() {
+    const querySnapshot = await getDocs(collection(db, `users_liked/${currentUser.uid}/posts`));
+
+    querySnapshot.forEach(async (doc) => {
+        const book = {...doc.data(), id: doc.id};
+        const bookCard = await createBookCard(book);
+        bookContainer.appendChild(bookCard);
+    });
+}
+
+async function createBookCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('book_card');
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('book_card_text_container')
+
+    const title = document.createElement('h1');
+    title.classList.add('book_card_title')
+    title.textContent = book.title.substring(0, 20);
+
+    const author = document.createElement('p');
+    author.classList.add('book_card_author')
+    author.innerHTML = `<b>Author(s):</b>${book.author}`;
+
+    const type = document.createElement('p');
+    type.classList.add('book_card_type')
+    type.innerHTML = `<b>Type:</b> ${book.type}`;
+
+    const pages = document.createElement('p');
+    pages.classList.add('book_card_pages')
+    pages.innerHTML = `<b>Pages:</b> ${book.pages}`;
+
+    const year = document.createElement('p');
+    year.classList.add('book_card_year')
+    year.innerHTML = `<b>Year of Publication</b>: ${book.year}`;
+
+    const isbn = document.createElement('p');
+    isbn.classList.add('book_card_isbn')
+    isbn.innerHTML = `<b>ISBN:</b> ${book.isbn}`;
+
+    const timeInfo = document.createElement('div');
+    timeInfo.classList.add('book_card_time_info')
+
+    const createdAt = document.createElement('p');
+    const updatedAt = document.createElement('p');
+    createdAt.classList.add('book_card_time_info_createdAt');
+    updatedAt.classList.add('book_card_time_info_updatedAt');
+    createdAt.innerHTML = `<small>Created at: ${book.createdAt}</small>`
+    updatedAt.innerHTML = `<small>Updated at: ${book.updatedAt}</small>`
+
+    timeInfo.append(createdAt, updatedAt)
+
+
+    const image = document.createElement('img');
+    image.classList.add('book_card_image')
+    image.src = book.imageUrl;
+    image.alt = book.title;
+
+    const controls_container = document.createElement('div');
+    controls_container.classList.add('controls_container');
+
+    const like_button = document.createElement('i');
+
+    like_button.classList.add('fa-solid');
+    like_button.classList.add('fa-heart');
+
+    const docSnap = await getDoc(doc(db, `users_liked/${currentUser.uid}/posts/`, `${book.id}`));
+
+    if (docSnap.exists()) {
+        like_button.classList.add('checked');
+    }
+
+    like_button.addEventListener('click', (e) => {
+        if (e.target.classList.contains('checked')) {
+            deleteDoc(doc(db, `users_liked/${currentUser.uid}/posts/`, `${book.id}`));
+        } else {
+            setDoc(doc(db, `users_liked/${currentUser.uid}/posts/`, `${book.id}`), book);
+        }
+
+        e.target.classList.toggle('checked');
+    })
+
+    controls_container.appendChild(like_button);
+    card.appendChild(image);
+    textContainer.appendChild(title);
+    textContainer.appendChild(author);
+    textContainer.appendChild(type);
+    textContainer.appendChild(pages);
+    textContainer.appendChild(year);
+    textContainer.appendChild(isbn);
+    textContainer.appendChild(timeInfo);
+    card.appendChild(textContainer);
+    card.appendChild(controls_container);
+
+    return card;
+}
 
 async function fillHeader() {
     const querySnapshot = await getDocs(collection(db, 'nav_items'));
@@ -36,5 +135,16 @@ async function fillHeader() {
     nav_list.innerHTML += '\n' + items['about_us'];
 }
 
-
+const bookContainer = document.getElementById('liked_books_container');
 const nav_list = document.getElementById('nav_list');
+
+
+const log_out_btn = document.querySelector('.log_out_button');
+
+log_out_btn.addEventListener('click', () => {
+    signOut(auth)
+        .then(() => {
+            location = '/sign_in';
+        }
+    )
+})
